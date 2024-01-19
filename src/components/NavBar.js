f381008261a96b309986b1790ba36514469de5ef
cgandotr <cgandotr@ucsc.edu>
2024-01-19 15:08:17 -0800

    styling options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './NavBar.css';
 
 function NavBar() {
@@ -7,11 +7,11 @@ function NavBar() {
         <div className='navbar'>
             <div className='logo'>MoCode</div>
             <div className='links'>
-                <Link to="/" id="home">Home</Link>
-                <Link to="/profile" id="profile">Profile</Link>
+                <NavLink to="/" id="home" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>Home</NavLink>
+                <NavLink to="/profile" id="profile" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>Profile</NavLink>
             </div>
         </div>
-    )
+    );
 }
 
 export default NavBar;
