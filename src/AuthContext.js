9aef21807afef9d9cdee9424e883e4e9248d5a51
cgandotr <cgandotr@ucsc.edu>
2024-01-31 17:51:01 -0800

    new user login

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -61,7 +61,7 @@ export const AuthProvider = ({ children }) => {
         };
 
         return () => unsubscribe();
-    }, []);
+    }, []); 
 
     return (
         <AuthContext.Provider value={{ currentUser, userProblems, problems, loading }}>
