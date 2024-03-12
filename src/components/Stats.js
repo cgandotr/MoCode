57578e26e4bd727e5fb61e2d0b9d3546a71d32d4
cgandotr <cgandotr@ucsc.edu>
2024-03-12 00:38:17 -0700

    ui

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,26 +1,35 @@
-import { useContext , useEffect, useState, React} from 'react';
-
+import { useContext , useEffect, useState, React } from 'react';
 import './Stats.css';
-import { AuthContext } from '../AuthContext'; // Adjust the path to your AuthContext
+
+/* AuthContext Imports */
+import { AuthContext } from '../AuthContext';
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import dayjs from 'dayjs';
-import isoWeek from 'dayjs/plugin/isoWeek'; // Import isoWeek plugin for dayjs
-
-import Badge from '@mui/material/Badge';
-import { PickersDay } from '@mui/x-date-pickers/PickersDay';
-
 import { PieChart } from '@mui/x-charts/PieChart';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-
-import Switch from '@mui/material/Switch';
 import SwipeableViews from 'react-swipeable-views';
 import { Tabs, Tab, Typography } from '@mui/material';
-import { tab } from '@testing-library/user-event/dist/tab';
+import Badge from '@mui/material/Badge';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+
+/* Other Imports */
+import dayjs from 'dayjs';
+
+/* Custom imports */
+import { categoryColors } from '../index';
 
 
+/*
+getWeekRange()
+------------------------------------
+Gets start and end dates for a given week number & year
+------------------------------------
+inputs: weekNumber (int)
+        year (int)
+
+outputs: [startofWeek, endofWeek] dates
+*/
 const getWeekRange = (weekNumber, year) => {
     const startOfWeek = dayjs().year(year).week(weekNumber).startOf('week');
     const endOfWeek = startOfWeek.clone().endOf('week');
@@ -28,17 +37,17 @@ const getWeekRange = (weekNumber, year) => {
 };
 
 
-
+/*
+TabPanel()
+------------------------------------
+Custom Object to help with Pie Chart/Calendar
+------------------------------------
+*/
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
   
     return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`full-width-tabpanel-${index}`}
-        {...other}
-      >
+      <div role="tabpanel" hidden={value !== index} id={`full-width-tabpanel-${index}`}{...other}>
         {value === index && (
           <Box p={3}>
             <Typography>{children}</Typography>
@@ -49,37 +58,17 @@ function TabPanel(props) {
   }
   
 
-
-
-const categoryColors = {
-    "Arrays & Hashing":  "#d65a5a",
-    "Two Pointers": "#d6855a",
-    "Sliding Window": "#d6b35a",
-    "Stack": "#b1d65a",
-    "Binary Search": "#5ad666",
-    "Linked List": "#757dd1",
-    "Trees": "#5a96d6",
-    "Tries": "#5a68d6",
-    "Heap / Priority Queue": "#815ad6",
-    "Backtracking": "#bd5ad6",
-    "Graphs": "#d65ab3",
-    "Advanced Graphs": "#d65a64",
-    "1-D Dynamic Programming": "#5B60D0",
-    "2-D Dynamic Programming": "#5B60D0",
-    "Greedy": "#5B60D0",
-    "Intervals": "#5B60D0",
-    "Math & Geometry": "#5B60D0",
-    "Bit Manipulation": "#5B60D0",
-    "JavaScript": "#5B60D0",
-};
-
-
-
+/*
+ServerDay()
+------------------------------------
+Custom Object to with streak indicator
+------------------------------------
+*/
 function ServerDay(props) {
     const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
-  
-    const isSelected =
-      !props.outsideCurrentMonth && highlightedDays.indexOf(props.day.date()) >= 0;
+
+    /* If current day is in highlighted days array, then we want to display streak */
+    const isSelected = !props.outsideCurrentMonth && highlightedDays.indexOf(props.day.date()) >= 0;
   
     return (
       <Badge
@@ -88,7 +77,7 @@ function ServerDay(props) {
         badgeContent={isSelected ? '🔥' : undefined}
         sx={{
             ...(isSelected && {
-              '.MuiPickersDay-root': { // Apply the background color to the day itself
+              '.MuiPickersDay-root': { 
                 backgroundColor: 'var(--boxes-background)'
               },
             }),
@@ -99,128 +88,203 @@ function ServerDay(props) {
     );
   }
 
-function Stats() {
-    
-    const [highlightedDays, setHighlightedDays] = useState([]);
 
+/*
+Stats
+------------------------------------
+Stats Component for User Home Page
+Displays:
+    Top 3 Questions
+    PieChart Data
+    Calendar w/ Streaks and hrs/week
+Used in 'Home' Component
+------------------------------------
+*/
+function Stats() {
+
+    /*
+    AuthContext Variables
+    */
     const { userProblems, problems } = useContext(AuthContext);
+
+    /*
+    State for highlighted day array
+    */
+    const [highlightedDays, setHighlightedDays] = useState([]);
     
+    /*
+    State for tab index
+    */
     const [tabvalue, setTabValue] = useState(0);
 
+    /*
+    State for calendar value
+    */
     const [calendarvalue, setCalendarValue] = useState(dayjs());
 
-  // Determine the start and end of the current month
-  const currentMonthStart = dayjs().startOf('month');
-  const currentMonthEnd = dayjs().endOf('month');
+    /*
+    State for Pie Chart Data
+    */
+    const [pieChartData, setPieChartData] = useState([]);
 
+    /* Determine start/end of current month
+       We only care about displaying current month info */
+    const currentMonthStart = dayjs().startOf('month');
+    const currentMonthEnd = dayjs().endOf('month');
 
-  const handleChange = (event, newValue) => {
-    setTabValue(newValue);
-  };
-
-  const handleChangeIndex = (index) => {
-    setTabValue(index);
-  };
-
-  useEffect(() => {
-    const getHighlightedDays = () => {
-        const currentMonthStart = dayjs().startOf('month');
-        const currentMonthEnd = dayjs().endOf('month');
-
-        // Initialize an empty array to store all highlighted days
-        let allHighlightedDays = [];
-
-        // Iterate through each userProblem
-        userProblems.forEach(problem => {
-            problem.status.forEach((status, index) => {
-                // Check if the status is "Complete" or "Incomplete"
-                if (status === "Complete" || status === "Incomplete") {
-                    // Convert the corresponding dateCompleted to dayjs and check if it's within the current month
-                    const attemptDate = dayjs(problem.dateCompleted[index].toDate());
-                    if (attemptDate.isAfter(currentMonthStart) && attemptDate.isBefore(currentMonthEnd)) {
-                        // If so, add this day to the allHighlightedDays array
-                        allHighlightedDays.push(attemptDate.date());
-                    }
-                }
-            });
-        });
-
-        // Deduplicate the days before returning
-        return [...new Set(allHighlightedDays)];
+    /*
+    handleChangeTab()
+    handleChangeSwipeableView()
+    ------------------------------------
+    Function that handles changing tabs
+    ------------------------------------
+    */
+    const handleChangeTab = (event, newValue) => {
+        setTabValue(newValue);
     };
 
-    setHighlightedDays(getHighlightedDays());
-}, [userProblems]); // Re-run this effect if userProblems changes
+    const handleChangeSwipeableView = (index) => {
+        setTabValue(index);
+    };
 
-        // Ensure userProblems is sorted by the length of their status array in descending order,
-        // but only count 'Complete' or 'Incomplete' statuses.
-        const sortedProblems = [...userProblems].sort((a, b) => {
-            // Calculate the count of 'Complete' or 'Incomplete' for each problem
-            const aCount = a.status.filter(status => status === "Complete" || status === "InComplete").length;
-            const bCount = b.status.filter(status => status === "Complete" || status === "InComplete").length;
-        
-            return bCount - aCount;
-        });
+    /*
+    useEffect()
+    ------------------------------------
+    Here we get the highlighted days
+    Highlighted Days should be days where a user has submited a problem
+    A submitted problem indicates a user has set that status as "Complete" or "InComplete"
+    Thus we get the set of days for which a user has submitted a problem
+    Gets reran if userProblem changes
+    ------------------------------------
+    */
+    useEffect(() => {
+        const getHighlightedDays = () => {
+            /* Initialize return array */
+            let allHighlightedDays = [];
 
-        // console.log(sortedProblems)
+            /* Iterate through all userProblems */
+            userProblems.forEach(problem => {
+                problem.status.forEach((status, index) => {
+                    /* Go through each status in status[] */
+                    /* Check if the status is "Complete" or "Incomplete" */
+                    if (status === "Complete" || status === "Incomplete") {
+                        /* Get corresponding dateCompleted (by index) */
+                        /* Check if it's within the current month */
+                        const attemptDate = dayjs(problem.dateCompleted[index].toDate());
+                        if (attemptDate.isAfter(currentMonthStart) && attemptDate.isBefore(currentMonthEnd)) {
+                            /* Add date to array */
+                            allHighlightedDays.push(attemptDate.date());
+                        }
+                    }
+                });
+            });
+
+            /* Return set of all dates */
+            return [...new Set(allHighlightedDays)];
+        };
+
+        /* Set the state of highlighted days */
+        setHighlightedDays(getHighlightedDays());
+    }, [userProblems]);
+
+
+    /*
+    sortedProblems
+    ------------------------------------
+        Array of userProblems that is sorted by the length of their status array (where status[i] == "Complete" or "InComplete")
+            i.e. sorted by how many submissions the user has had for a problem
+        We will use this array to determine the top3Problems
+    */
+    const sortedProblems = [...userProblems].sort((a, b) => {
+        /* Calculate the count of 'Complete' or 'Incomplete' for each problem*/
+        const aCount = a.status.filter(status => status === "Complete" || status === "InComplete").length;
+        const bCount = b.status.filter(status => status === "Complete" || status === "InComplete").length;
+        return bCount - aCount;
+    });
+
         
-        // Select top 3 problems
-        const top3Problems = sortedProblems.slice(0, 3);
+    /*
+    top3Problems
+    ------------------------------------
+        Array of first 3 userProblems from sortedProblems
+    */
+    const top3Problems = sortedProblems.slice(0, 3);
   
 
-    // Find the problem title based on the link
+    /*
+    getProblemTitle()
+    ------------------------------------
+    Gets the problem title by matching the given problem link
+    Searches problems to find this match
+    Returns ... if no match
+    ------------------------------------
+    inputs: problemLink (string)
+
+    outputs: title (string)
+    */
     const getProblemTitle = (problemLink) => {
         const matchedProblem = problems.find(p => p.link === problemLink);
         return matchedProblem ? matchedProblem.title : '...';
     };
 
-
-
-    const [pieChartData, setPieChartData] = useState([]);
-
-
+    /*
+    useEffect()
+    ------------------------------------
+    Here we get the Pie Chart Data
+    We determine the frequency of categories where a user has completed a problem for that category
+     Gets reran if userProblem changes
+    ------------------------------------
+    */
     useEffect(() => {
-        // Calculate category frequencies based on completed problems
+        /* Calculate category frequencies based on completed problems */
         const categoryFrequencies = userProblems.reduce((acc, curr) => {
-            // Check if the problem has been completed at least once
+            /* Check if the problem has been completed at least once */
             if (curr.status.includes("Complete")) {
-                // Find the problem to get its category
+                /* Find the problem to get its category */
                 const problem = problems.find(p => p.link === curr.problemLink);
                 const category = problem ? problem.category : 'Unknown';
-                // Increment the count for this category
+                /* Increment the count for this category */
                 acc[category] = (acc[category] || 0) + 1;
             }
             return acc;
         }, {});
-    
-        // Convert the frequencies to data the PieChart can use
+        
+        /* Convert the frequencies to data the PieChart can use */
         const chartData = Object.keys(categoryFrequencies).map((category, index) => ({
             id: index,
             value: categoryFrequencies[category],
             label: category,
-            color: categoryColors[category] // Optional: Assign a color for each category if desired
+            color: categoryColors[category]
         }));
-    
+        /* Set the state of Pie Chart Data */
         setPieChartData(chartData);
     }, [userProblems, problems]);
 
-    const renderDay = (day, selectedDates, pickersDayProps) => {
-        return <div>hi</div>
-      };
     
-      const calculateHoursSpent = (weekNumber) => {
-        // Make sure to call .year() to get the current year as a number
+    /*
+    calculateHoursSpent()
+    ------------------------------------
+    Gets the total number of hours given by timeDuration for a given week
+    ------------------------------------
+    inputs: weekNumber (int)
+
+    outputs: hours (int)
+    */
+    const calculateHoursSpent = (weekNumber) => {
+        /* Get the week Range */
         const { startOfWeek, endOfWeek } = getWeekRange(weekNumber, dayjs().year());
-    
+
         const secondsInHour = 3600;
         
         const hours = userProblems.reduce((totalHours, problem) => {
-            // Iterate over the status array for each problem
+            /* Iterate over the status array for each problem */
             return problem.status.reduce((acc, currentStatus, index) => {
                 if (currentStatus === "Complete" || currentStatus === "InComplete") {
+                    /* We only care about submitted problems */
                     const completionDate = dayjs(problem.dateCompleted[index].toDate());
+                    /* Check if within range */
                     if (completionDate.isAfter(startOfWeek) && completionDate.isBefore(endOfWeek)) {
-                        // Convert seconds to hours and accumulate
+                        /* Convert seconds to hours and accumulate */
                         const hoursForProblem = problem.timeDuration[index] / secondsInHour;
                         return acc + hoursForProblem;
                     }
@@ -228,17 +292,12 @@ function Stats() {
                 return acc;
             }, totalHours);
         }, 0);
-    
-        // Round the total hours to the nearest whole number
+
+        /*  Round the total hours to the nearest whole number */
         const roundedHours = Math.round(hours);
-        
-        console.log(roundedHours);
         return roundedHours;
-    };
-    
-    
-    
-    
+    };  
+
    
     return (
         <div className='stats'>
@@ -263,7 +322,7 @@ function Stats() {
             <div id="bottom-stats">
                 <Tabs 
                     value={tabvalue}
-                    onChange={handleChange}
+                    onChange={handleChangeTab}
                     variant="fullWidth"
                     sx={{
                         '& .MuiTabs-indicator': {backgroundColor: 'var(--main-font-color)'},
@@ -273,12 +332,12 @@ function Stats() {
                       }}
                 >
                     <Tab label="Category" />
-                    <Tab label="Calender" />
+                    <Tab label="Calendar" />
                 </Tabs>
                 <SwipeableViews
                     axis={'x'}
                     index={tabvalue}
-                    onChangeIndex={handleChangeIndex}
+                    onChangeIndex={handleChangeSwipeableView}
                 >
                     <TabPanel value={tabvalue} index={0}>
                             <PieChart id="pie"
@@ -311,7 +370,6 @@ function Stats() {
                             renderInput={() => <></>} // Render nothing or your custom input component
                             minDate={currentMonthStart}
                             maxDate={currentMonthEnd}
-                            renderDay={renderDay}
                             slots={{
                                 day: ServerDay,
                             }}
